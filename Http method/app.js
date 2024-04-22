fe48512e648735744191f5d19755265158445461
unknown <jamilafzal255@gmail.com>
2024-04-22 22:34:50 +0500

    put request

--- Http method/app.js
+++ Http method/app.js
@@ -1,27 +1,36 @@
 import express from 'express'
 import data from "file:///D:/Backend/Http%20method/data.json" assert { type: "json" };
 import fs from 'fs'
+import bodyParser from 'body-parser'
 const app = express()
 const PORT = 3000
-app.use(express.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.listen(PORT, (req, res) => console.log(`server runing  on ${PORT}`))
 
-app
-    .route('/data/:id')
+app.route("/data/:id")
     .get((req, res) => {
-        const singleProduct = data.find((p) => p.id === +req.params.id)
+        const singleProduct = data.find(p => p.id === +req.params.id)
         res.send(singleProduct)
     })
-    .put((req, res) => res.json({ status: 'pending' }))
-    .delete((req, res) => res.json({ status: 'pending' }))
-    .patch((req, res) => res.json({ status: 'pending' }))
+    .put((req, res) => {
+        console.log(req.body);
+        const body = req.body
+        data.push({ ...body, id: data.length + 1 })
+        fs.writeFile("./data.json", JSON.stringify(data), (err, data) => { })
+        res.json({ status: "pending" })
+    })
+    .delete((req, res) => {
+        res.json({ status: "pending" })
+    })
+    .patch((req, res) => {
+        res.json({ status: "pending" })
+    })
 
-app.post('/add/data', (req, res) => {
+app.post("/add/data", (req, res) => {
     const body = req.body
-    data.push({ ...body, id: data.length+1 })
-    fs.writeFile("./data.json", JSON.stringify(data), (err, data) => { 
+    data.push({ ...body, id: data.length + 1 })
+    fs.writeFile("./data.json", JSON.stringify(data), (error, data) => {
+        return res.json({ status: "success", id: data.length })
 
-        res.json({ status: 'sucess',id: data.length})
     })
 })
-
-app.listen(PORT, (req, res) => console.log(`server runing  on ${PORT}`))
