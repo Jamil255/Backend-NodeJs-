a894c1309253995974c272d0956228cfd77264d2
unknown <jamilafzal255@gmail.com>
2024-04-23 07:56:38 +0500

    status code explore and handle

--- Http method/app.js
+++ Http method/app.js
@@ -5,15 +5,29 @@ import bodyParser from 'body-parser'
 const app = express()
 const PORT = 3000
 app.use(bodyParser.json());
+
+const authMiddleWare = (req, res, next) => {
+
+    const isAuth = true
+    !isAuth ? res.status(401).send({
+        message: "unauthorized user"
+    }) : next()
+}
 app.listen(PORT, (req, res) => console.log(`server runing  on ${PORT}`))
 
 app.route("/data/:id")
-    .get((req, res) => {
+    .get(authMiddleWare, (req, res) => {
+        // Headers
+        // res.setHeader("X-server", "localhost:300/data/:id")
+        // // alway add to X custom headers 
+        // const header = req.headers
+        //    to see request headers 
+        // console.log(header);
         const singleProduct = data.find(p => p.id === +req.params.id)
-        res.send(singleProduct)
+        if (!data.id) return res.status(404).json({ message: "product not found" })
+        res.status(200).send(singleProduct)
     })
     .put((req, res) => {
-        console.log(req.body);
         const body = req.body
         data.push({ ...body, id: data.length + 1 })
         fs.writeFile("./data.json", JSON.stringify(data), (err, data) => { })
@@ -28,9 +42,13 @@ app.route("/data/:id")
 
 app.post("/add/data", (req, res) => {
     const body = req.body
-    data.push({ ...body, id: data.length + 1 })
-    fs.writeFile("./data.json", JSON.stringify(data), (error, data) => {
-        return res.json({ status: "success", id: data.length })
+    if (!body || !body.title || !body.price || !body.description || !body.category || !body.image) {
+        return res.status(400).json({
+            message: "all  parameters passed"
+        })
 
-    })
+    }
+    data.push({ ...body, id: data.length + 1 })
+    fs.writeFile("./data.json", JSON.stringify(data), (error, data) => { })
+    return res.status(201).json({ status: "success", id: data.length })
 })
