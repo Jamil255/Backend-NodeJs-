2191fa928098709714a587c479895ab14f9dec14
unknown <jamilafzal255@gmail.com>
2024-05-14 16:47:27 +0500

    login api

--- jwt/controllers/userController.js
+++ jwt/controllers/userController.js
@@ -1,6 +1,7 @@
 import userModel from '../models/userSchema.js'
 import bcrypt, { hash } from 'bcrypt'
 import nodemailer from 'nodemailer'
+import jwt from 'jsonwebtoken'
 import { EmailVerificationHtml } from '../template/index.js'
 import OtpModel from '../models/OtpSchema.js'
 import { response } from 'express'
@@ -35,7 +36,7 @@ const handleSignUpFun = async (req, res) => {
       from: process.env.GMAIL_ACCOUNT,
       to: email,
       subject: 'Email Verfication',
-      html: EmailVerificationHtml(otp),
+      html: EmailVerificationHtml(otp, userRes?.fullName),
     })
     await OtpModel.create({
       otp,
@@ -54,7 +55,53 @@ const handleSignUpFun = async (req, res) => {
     })
   }
 }
-const handleSignInFun = () => {}
+const handleSignInFun = async (req, res) => {
+  try {
+    const { email, password } = req.body
+    if (!email || !password) {
+      res.json({
+        message: 'input field  are required',
+        status: false,
+      })
+      return
+    }
+
+    const user = await userModel.findOne({ email })
+    if (!user) {
+      res.json({
+        message: 'email and password is incorrect',
+        status: false,
+      })
+      return
+    }
+    const comparePass = await bcrypt.compare(password, user.password)
+    if (!comparePass) {
+      res.json({
+        message: 'email and password  invalid',
+        status: false,
+      })
+      return
+    }
+    const token = await jwt.sign(
+      {
+        _id: user._id,
+        email: user.email,
+      },
+      process.env.PRIVATE_KEY
+    )
+    res.json({
+      message: 'Your account has been successfully login',
+      status: true,
+      token,
+    })
+  } catch (error) {
+    res.json({
+      message: error.message,
+      status: false,
+      data: [],
+    })
+  }
+}
 const handleOtpVerifyFun = async (req, res) => {
   try {
     const { email, otp } = req.body
