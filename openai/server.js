02d41dba9b7148945e7affb97957b007793ba255
unknown <jamilafzal255@gmail.com>
2024-06-02 19:25:23 +0500

    429 too many request

--- openai/server.js
+++ openai/server.js
@@ -1,8 +1,23 @@
 import express from 'express'
-import routes from './routes/index.js'
+import OpenAI from 'openai'
+import dotenv from 'dotenv'
 const app = express()
+dotenv.config()
 const PORT = process.env.PORT || 3000
-app.use(routes)
+const openai = new OpenAI({
+  apiKey: process.env.OPEN_AI_KEY,
+})
+
+async function main() {
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: 'user', content: 'Say this is a test' }],
+    model: 'gpt-3.5-turbo',
+  })
+  console.log(chatCompletion)
+}
+
+main()
+
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`)
 })
