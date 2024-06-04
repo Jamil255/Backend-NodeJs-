6070bb5aa1440a3dc2c1cfd7b2e470007d7178ad
unknown <jamilafzal255@gmail.com>
2024-06-04 17:47:54 +0500

    streams

--- Streams/server.js
+++ Streams/server.js
@@ -1,8 +1,23 @@
 import express from 'express'
-
+import fs from 'fs'
+import status from 'express-status-monitor'
 const app = express()
-
+app.use(status())
 const PORT = process.env.PORT || 3000
+// momery consumers
+// app.get('/', (req, res) => {
+//   fs.readFile('./data.txt', (error, data) => {
+//     res.end(data)
+//   })
+// })
+
+// streams
+app.get('/', (req, res) => {
+  const stream = fs.createReadStream('./data.txt', 'utf-8')
+    //   console.log(stream)
+    stream.on("data", (chunk) => res.write(chunk))
+    stream.on("end", () =>res.end())
+})
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`)
