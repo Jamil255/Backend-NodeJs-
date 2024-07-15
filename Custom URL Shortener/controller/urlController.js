173fec7e2287c2cbebb1156ee3433b34c87492a4
JAMIL AFZAL <134855468+Jamil255@users.noreply.github.com>
2024-07-15 15:17:27 +0500

    Update urlController.js

--- Custom URL Shortener/controller/urlController.js
+++ Custom URL Shortener/controller/urlController.js
@@ -4,6 +4,7 @@ const shortId = generateShortId()
 const handleGenericShortUrl = async (req, res) => {
   try {
     const body = req.body
+    console.log(body)
     if (!body.url) return res.status(404).json({ error: 'url is required' })
     const response = await URL.create({
       shortId: shortId,
