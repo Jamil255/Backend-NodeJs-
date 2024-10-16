8dcbd24096ce2654cc616308ea4d5ab407a5edba
unknown <jamilafzal255@gmail.com>
2024-10-16 21:09:49 +0500

    db connect

--- auth/server/index.js
+++ auth/server/index.js
@@ -1,5 +1,16 @@
 import express from 'express'
+import mongoose from 'mongoose'
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 const PORT = process.env.PORT || 3000
-app.get('/', (req, res) => res.json({ status: 'success' }))
+const connectDb = async () => {
+  try {
+    const conn = await mongoose.connect()
+    console.log(conn.connection.host)
+  } catch (error) {
+    console.log(error)
+  }
+}
+connectDb()
 app.listen(PORT, () => console.log('`port` is runing at http://localhost:300'))
