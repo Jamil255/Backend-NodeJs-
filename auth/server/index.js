6d7876ad02711f0d36900301a729ec45aec341d9
unknown <jamilafzal255@gmail.com>
2024-10-18 11:58:36 +0500

    auth

--- auth/server/index.js
+++ auth/server/index.js
@@ -2,9 +2,11 @@ import express from 'express'
 import mongoose from 'mongoose'
 import connectDb from './config/db.js'
 import routes from './routes/index.js'
+import cors from 'cors'
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors({ origin: 'http://localhost:5173' }))
 connectDb()
 app.use(routes)
 const PORT = process.env.PORT || 3000
