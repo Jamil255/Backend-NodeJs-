a32e74fb448920b01906f3f4126b587ea11160db
JAMIL AFZAL <134855468+Jamil255@users.noreply.github.com>
2024-10-19 17:31:55 +0500

    Update index.js

--- auth/server/index.js
+++ auth/server/index.js
@@ -8,6 +8,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({ origin: 'http://localhost:5173' }))
 connectDb()
+
 app.use(routes)
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log('`port` is runing at http://localhost:300'))
